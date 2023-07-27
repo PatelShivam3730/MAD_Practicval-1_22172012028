6edf7ad70db7b46ea7458d505776170bac572b27
PatelShivam3730 <139306818+PatelShivam3730@users.noreply.github.com>
2023-07-27 20:32:59 +0530

    Update practical-1.9.kt

--- src/main/kotlin/practical-1.9.kt
+++ src/main/kotlin/practical-1.9.kt
@@ -1,4 +1,37 @@
-fun main()
-{
-    
+fun main() {
+    val numberList = readNumberListFromUser(5) // Specify the number of values (5) here
+
+    val maxNumber = numberList.maxOrNull()
+
+    if (maxNumber != null) {
+        println("The maximum number is: $maxNumber")
+    } else {
+        println("The list is empty.")
+    }
 }
+
+fun readNumberListFromUser(count: Int): ArrayList<Int> {
+    val numberList = arrayListOf<Int>()
+
+    println("Enter $count numbers:")
+    var input: String?
+    var inputCount = 0
+
+    while (inputCount < count) {
+        input = readLine()
+        if (input == null || input == "q") {
+            println("Invalid input. Please enter a valid number.")
+            continue
+        }
+
+        val number = input.toIntOrNull()
+        if (number != null) {
+            numberList.add(number)
+            inputCount++
+        } else {
+            println("Invalid input. Please enter a valid number.")
+        }
+    }
+
+    return numberList
+}￼Enter
